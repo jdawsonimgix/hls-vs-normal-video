5b3b578b2cf372cc84beadbe0dc5940c86d2a285
Jamie Dawson <Jamie@imgixs-mbp.attlocal.net>
2021-12-28 17:05:53 -0800

    Added video players and search bars. Next is to add button to play both videos at once

--- hls-vs-normal-video/src/App.js
+++ hls-vs-normal-video/src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ReactHlsPlayer from 'react-hls-player';
 
 
 function App() {
+  //const [urlOne, handleOnChange] = useState('https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8');
+  const [urlHLS, setName] = useState("");
+  const [urlOther, setNameOther] = useState("");
+  //const [trueOrFalse, trueOrFalseButton] = useState("false");
+
+
+
   return (
     <div className="App">
       <header className="App-header">
+ 
+      <form>
+      <label>Enter an HLS url:
+        <input
+          type="text" 
+          value={urlHLS}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+    <form>
+      <label>Enter a non-HLS url:
+        <input
+          type="text" 
+          value={urlOther}
+          onChange={(e) => setNameOther(e.target.value)}
+        />
+      </label>
+    </form>
+
+    <p>{urlHLS}</p>
+
+    {urlHLS.length > 0 && 
       <ReactHlsPlayer
-      src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-      autoPlay={false}
-      controls={true}
-      width="40%"
-      height="auto"
-  />
+        src={urlHLS}
+        autoPlay={false}
+        controls={true}
+        width="40%"
+        height="auto"
+     /> }
+
+    <p>{urlOther}</p>
+
+    {urlOther.length > 0 && 
+      <ReactHlsPlayer
+        src={urlOther}
+        autoPlay={false}
+        controls={true}
+        width="40%"
+        height="auto"
+    /> }
 
 
-      </header>
+
+    </header>
+    <button >Click to play both videos</button>
+
     </div>
   );
 }
 
 export default App;
+
+
+/*
+Example url:   https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8
+package:  https://www.npmjs.com/package/react-hls-player
+*/
