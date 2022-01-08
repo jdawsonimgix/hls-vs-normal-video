273b3ec5067630068044c927b5be1cebda82cb4a
Jamie Dawson <Jamie@imgixs-mbp.attlocal.net>
2022-01-07 16:48:07 -0800

    Plays mp4 and playing true or false functionaltiy works. I now need to connect it to a button to trigger the state

--- hls-vs-normal-video/src/App.js
+++ hls-vs-normal-video/src/App.js
@@ -1,44 +1,46 @@
 import React, { useState } from 'react';
 import './App.css';
 import ReactHlsPlayer from 'react-hls-player';
+import ReactPlayer from 'react-player'
+
 
 
 function App() {
-  //const [urlOne, handleOnChange] = useState('https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8');
-  const [urlHLS, setName] = useState("");
-  const [urlOther, setNameOther] = useState("");
-  //const [torf, trueOrFalseButton] = useState("false");
-  const playerRef = React.useRef();
+  const [urlHLS, setHLS] = useState(""); //First url that is an HLS file
+  const [urlOther, setOtherUrl] = useState(""); //Second url that is a non HLS file
+  const playerRef = React.useRef(); //Gets a ref of the HLS video and plays it.
+  const [mp4PlayValue, setMp4PlayValue] = useState({playing: false})
+
+  console.log(mp4PlayValue.playing)
 
   function playVideo() {
-    playerRef.current.play();
+    playerRef.current.play();  
   }
 
   return (
     <div className="App">
       <header className="App-header">
- 
+
       <form>
-      <label>Enter an HLS url:
+      <label>Enter HLS url:
         <input
           type="text" 
           value={urlHLS}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setHLS(e.target.value)}
         />
       </label>
     </form>
     <form>
-      <label>Enter a non-HLS url:
+      <label>Enter mp4 url:
         <input
           type="text" 
           value={urlOther}
-          onChange={(e) => setNameOther(e.target.value)}
+          onChange={(e) => setOtherUrl(e.target.value)}
         />
       </label>
     </form>
 
-    <p>{urlHLS}</p>
-
+  
     {urlHLS.length > 0 && 
       <ReactHlsPlayer
         playerRef={playerRef}
@@ -46,24 +48,23 @@ function App() {
         autoPlay={false}
         controls={true}
         width="40%"
-        height="auto"
+        height="40%"
      /> }
 
-    <p>{urlOther}</p>
-
-    {urlOther.length > 0 && 
-      <ReactHlsPlayer
-        src={urlOther}
-        autoPlay={false}
-        controls={true}
-        width="40%"
-        height="auto"
-    /> }
+  <ReactPlayer 
+      url={urlOther} 
+      width='40%'
+      height='40%'
+      controls={true}
+      playing={mp4PlayValue.playing}
+    />
 
 
 
     </header>
-    <button onClick={() => playVideo()}>True or False</button>
+    <button onClick={() => playVideo() }>Play HLS</button>
+    <button onClick={() => setMp4PlayValue() }>Play MP4</button>
+
 
     </div>
   );
@@ -73,12 +74,21 @@ export default App;
 
 
 /*
-Example url:   https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8
-package:  https://www.npmjs.com/package/react-hls-player
+HLS urls:   https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8
+npm:  https://www.npmjs.com/package/react-hls-player
 
+
+MP4 urls:  http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4
+More urls:  https://gist.github.com/jsturgis/3b19447b304616f18657
+npm:  https://www.npmjs.com/package/react-player#supported-media
 
 Example urls:
 https://jdawson.imgix.video/mixkit-stars-in-space-1610.mp4?fm=hls
+
+
+Resource to add play functionality to mp4 video:  https://www.youtube.com/watch?v=Y-OLcnr8eNo
+
+
 */
 
 
