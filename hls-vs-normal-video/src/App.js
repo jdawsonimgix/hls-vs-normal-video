ac3bffc6a95eb404b0efa2dce0847da7700e4cc7
Jamie Dawson <Jamie@imgixs-mbp.attlocal.net>
2022-01-21 09:50:07 -0800

    updated code

--- hls-vs-normal-video/src/App.js
+++ hls-vs-normal-video/src/App.js
@@ -5,8 +5,12 @@ import ReactPlayer from "react-player";
 import { Nav, Navbar, Container } from "react-bootstrap";
 
 function App() {
-  const [urlHLS, setHLS] = useState(""); //First url that is an HLS file
-  const [urlOther, setOtherUrl] = useState(""); //Second url that is a non HLS file
+  const [urlHLS, setHLS] = useState(
+    "https://tom.imgix.video/s3toixjs.mp4?fm=hls"
+  ); //First url that is an HLS file
+  const [urlOther, setOtherUrl] = useState(
+    "https://tom.imgix.net/s3toixjs.mp4"
+  ); //Second url that is a non HLS file
   const playerRef = React.useRef(); //Gets a ref of the HLS video and plays it.
   const [mp4PlayValue, setMp4PlayValue] = useState({ playing: false });
 
@@ -58,19 +62,17 @@ function App() {
 
       <header className='App-header'>
         <p class='regular-text'>
-          This app is used to showcase the user experience of playing playing an
-          mp4 video vs an HLS video hosted on imgix.
+          This app is used to showcase the user experience of playing an mp4 vs
+          an HLS video with imgix. To test this, right-click on the browser and
+          select "inspect", click the Network tab, change the speed to Fast 3G,
+          and make sure the "disable cache" button next to it is selected.
         </p>
 
-        <p className='regular-text-other'>Example HSL links:</p>
-        <p className='link-text'>
-          https://jdawson.imgix.video/mixkit-stars-in-space-1610.mp4?fm=hls
-        </p>
+        <p className='regular-text-other'>Example HLS links:</p>
+        <p className='link-text'>https://tom.imgix.net/s3toixjs.mp4?fm=hls</p>
 
         <p className='regular-text-other'>Example MP4 links:</p>
-        <p className='link-text'>
-          http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4
-        </p>
+        <p className='link-text'>https://tom.imgix.net/s3toixjs.mp4</p>
 
         {urlHLS.length > 0 && (
           <ReactHlsPlayer
